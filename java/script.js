e8a43f5f31c89ac61532c0c0a2c983651c556c79
zmeller <zmeller@students.northweststate.edu>
2022-10-25 20:11:54 -0400

    second  push

--- java/script.js
+++ java/script.js
@@ -1,7 +1,7 @@
 
 window.onload = "/img/magic8ball_empty.png";
 
-let myPix = new Array("img/magic8ball_1.png", "img/magic8ball_2.png", "img/magic8ball_3.png", "img/magic8ball_4.png", "img/magic8ball_5.png", "img/magic8ball_6.png", "img/magic8ball_7.png", "img/magic8ball_8.png", "img/magic8ball_9.png", "img/magic8ball_10.png", "img/magic8ball_11.png", "img/magic8ball_12.png", "img/magic8ball_13.png", "img/magic8ball_14.png", "img/magic8ball_15.png", "img/magic8ball_16.png", "img/magic8ball_17.png", "img/magic8ball_18.png", "img/magic8ball_19.png" );;
+let myPix = new Array("img/magic8ball_1.png", "img/magic8ball_2.png", "img/magic8ball_3.png", "img/magic8ball_4.png", "img/magic8ball_5.png", "img/magic8ball_6.png", "img/magic8ball_7.png", "img/magic8ball_8.png", "img/magic8ball_9.png", "img/magic8ball_10.png", "img/magic8ball_11.png", "img/magic8ball_12.png", "img/magic8ball_13.png", "img/magic8ball_14.png", "img/magic8ball_15.png", "img/magic8ball_16.png", "img/magic8ball_17.png", "img/magic8ball_18.png", "img/magic8ball_19.png" );
  
 onchange = function choosePic() {
 	if (question.value.length < 1){
