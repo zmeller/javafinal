a747c42edcd97cda268913bf29bb49420d79c2d3
zmeller <zmeller@students.northweststate.edu>
2022-10-26 20:42:39 -0400

    added comments to code

--- java/script.js
+++ java/script.js
@@ -1,12 +1,14 @@
 const [form] = document.forms;
 const [askValidationMsg] = document.querySelectorAll('.validationMsg');
 
+/* variable declaration */
 const myPics = ["img/magic8ball_1.png","img/magic8ball_2.png", "img/magic8ball_3.png", "img/magic8ball_4.png", "img/magic8ball_5.png", "img/magic8ball_6.png", "img/magic8ball_7.png", "img/magic8ball_8.png", "img/magic8ball_9.png", "img/magic8ball_10.png", "img/magic8ball_11.png", "img/magic8ball_12.png", "img/magic8ball_13.png", "img/magic8ball_14.png", "img/magic8ball_15.png", "img/magic8ball_16.png", "img/magic8ball_17.png", "img/magic8ball_18.png", "img/magic8ball_19.png"];
 
+/* checking question character length */
 function isQuestionValid(question){
   return question.length > 10 && question.length < 100;
 }
-
+/* function validation for question length */
 function getElement(name, e){
   return {
     question(e){
@@ -16,6 +18,7 @@ function getElement(name, e){
   }[name](e);
 }
 
+/*grabbing user input*/
 function handleInput(e){
   const { question, askBtn } = form
   const { name } = e.target;
@@ -23,6 +26,7 @@ function handleInput(e){
   askBtn.disabled = !isQuestionValid(question.value);
 }
 
+/* listening for button submission, generates random number/image */
 document.addEventListener('DOMContentLoaded', () => {
   form.question.addEventListener('input', handleInput);
   form.addEventListener('submit', e => {
